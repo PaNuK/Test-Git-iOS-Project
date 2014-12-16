f5e559e3e979a81075509afc2dab9daa20520367
Giovan Gentile <giovan_gentile@hotmail.com>
2014-12-16 17:31:51 -0500

    added Test class

---
+++ GitTest/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  GitTest
+//
+//  Created by Giovan Gentile on 2014-12-16.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ GitTest/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  GitTest
+//
+//  Created by Giovan Gentile on 2014-12-16.
+//  Copyright (c) 2014 GG. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
